ef09060f0f78b927fff1fb97c70d946b043853aa
Iyanu Adelekan <iyanu.adelekan@konga.com>
2018-08-20 13:15:40 +0100

    Made update to graph data structure

--- src/structures/Graph.java
+++ src/structures/Graph.java
@@ -3,6 +3,12 @@ package structures;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Graph data structure implementation utilizing adjacency
+ * lists.
+ *
+ * @param <T> - the type of value held by each node within the graph.
+ */
 public class Graph<T> {
 
     Graph() {
@@ -15,9 +21,26 @@ public class Graph<T> {
         return nodes;
     }
 
+    /**
+     * Class modelling graph nodes.
+     *
+     * @param <U> - the type of value held within the node.
+     */
     private class Node<U> {
 
         private U value;
         private List<Node<U>> children;
+
+        void addChild(final Node<U> node) {
+            children.add(node);
+        }
+
+        List<Node<U>> getChildren() {
+            return children;
+        }
+
+        U getValue() {
+            return value;
+        }
     }
 }
