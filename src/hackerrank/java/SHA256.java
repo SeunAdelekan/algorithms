7ce19026d115aa669bcad683089b6ebeb1e8ef50
Iyanu Adelekan <iyanu.adelekan@konga.com>
2019-02-13 10:22:33 +0100

    SHA256

---
+++ src/hackerrank/java/SHA256.java
@@ -0,0 +1,28 @@
+package hackerrank.java;
+
+import java.security.MessageDigest;
+
+public class SHA256 {
+
+    public static void main(String[] args) {
+        System.out.println(getSha256("Iyanu Adelekan"));
+    }
+
+    public static String getSha256(String value) {
+        try{
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(value.getBytes());
+            return bytesToHex(md.digest());
+        } catch(Exception ex){
+            throw new RuntimeException(ex);
+        }
+    }
+    private static String bytesToHex(byte[] bytes) {
+        StringBuilder result = new StringBuilder();
+
+        for (byte b : bytes) {
+            result.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
+        }
+        return result.toString();
+    }
+}
