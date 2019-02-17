8155021ae0e8a82e1e643c663dd22440be2004de
Iyanu Adelekan <iyanuadelekan@gmail.com>
2019-02-17 17:53:25 +0100

    Update

--- src/hackerrank/java/MD5.java
+++ src/hackerrank/java/MD5.java
@@ -1,7 +1,27 @@
 package hackerrank.java;
 
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+import java.security.MessageDigest;
+
 /**
  * @author Iyanu Adelekan on 13/02/2019.
  */
 public class MD5 {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        final Scanner scanner = new Scanner(System.in);
+        final String str = scanner.nextLine();
+        final MessageDigest digest = MessageDigest.getInstance("MD5");
+        final StringBuilder buffer = new StringBuilder();
+        scanner.close();
+
+        final byte[] bytes = digest.digest(str.getBytes());
+
+        for (byte val : bytes) {
+            buffer.append(Integer.toHexString((val & 0xFF) | 0x100).substring(1, 3));
+        }
+
+        System.out.println(buffer.toString());
+    }
 }
